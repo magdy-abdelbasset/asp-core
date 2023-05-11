b1ecef20f13dfe2c0095539e0467c5298f8b033c
magdy-abdelbasset <89164324+magdy-abdelbasset@users.noreply.github.com>
2023-05-11 17:55:19 +0300

    view

---
+++ Models/Repostries/AuthorRepostry.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreDemo.Models.Repostries
+{
+    public class AuthorRepostry :IauthorstoreRepostry<Author>
+    {
+        List<Author> authors;
+        public AuthorRepostry(){
+            authors = new List<Author>(){
+                new Author {
+                    Id =1,FullName="sccsdcsdds",
+                },
+                new Author {
+                    Id =2,FullName="sccsdcsdds",
+                },
+            };
+        }
+        public IList<author> List(){
+            return authors;
+        }
+        public author Find(int Id){
+            return authors.SingleOrDefault(b => b.Id == id);
+        }
+        public void Add(TEntity entity){
+            authors.Add(entity);
+        }
+        public void Update(int id ,TEntity entity){
+           author =  Find(id)
+           author.Title = entity.Title;
+           author.Descreption = entity.Descreption;
+           author.Author = entity.Author;
+        }
+        public void Delete(int Id){
+           authors.Remove(Find(id));
+        }
+    }
+}
